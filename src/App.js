7fe6b55ace97d266bb3f5c8823b1e1d0e2653315
Matt Glissmann <mdglissmann@gmail.com>
2019-06-26 09:40:48 -0600

    moving playlist stats containers to modules

--- src/App.js
+++ src/App.js
@@ -15,69 +15,10 @@ import {
 import { FormClose, Notification } from 'grommet-icons';
 import queryString from 'query-string';
 import theme from './Themes/theme'
-
-const AppBar = (props) => ( 
-  <Box 
-    tag="header"
-    direction="row"
-    align="center"
-    justify="between"
-    background="brand"
-    pad={{ left: 'medium', right: 'small', vertical: 'small'}}
-    elevation="medium"
-    style={{ zIndex: '1'}}
-    {...props}
-  />
-)
-
-class PlaylistsNum extends Component {
-  render() {
-    return (
-      <Box>
-        <h2>{this.props.playlists.length} Playlists</h2>
-      </Box>
-    )
-  }
-}
-
-class PlaylistsSongs extends Component {
-  render() {
-    let allSongs = this.props.playlists.reduce( (songs, playlist) => {
-      return songs.concat(playlist.songs)
-    }, [])
-    return (
-      <Box>
-        <h2>{allSongs.length} Songs</h2>
-      </Box>
-    )
-  }
-}
-
-class PlaylistsTime extends Component {
-  render() {
-    let allSongs = this.props.playlists.reduce( (songs, playlist) => {
-      return songs.concat(playlist.songs)
-    }, [])
-    let totalPlayTime_ms = allSongs.reduce((playTime, song) => {
-      if (song.track.duration_ms) {
-        return playTime + song.track.duration_ms 
-      } else {
-        return playTime
-      }
-    }, 0)
-    let totalPlayTime_minutes = (totalPlayTime_ms / 1000) / 60
-
-    return (
-      <Box>
-        {
-          totalPlayTime_minutes > 60 
-            ? <h2>{Math.round(totalPlayTime_minutes / 60)} Hours</h2>
-            : <h2>{Math.round(totalPlayTime_minutes)} Minutes</h2>
-        }
-      </Box>
-    )
-  }
-}
+import { AppBar } from './Components/AppBar'
+import PlaylistsNum from './Containers/PlaylistsNum'
+import PlaylistsSongs from './Containers/PlaylistsSongs'
+import PlaylistsTime from './Containers/PlaylistsTime'
 
 class PlaylistsStats extends Component {
   render() {
